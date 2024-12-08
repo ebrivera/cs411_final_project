a41ce5356822439bf8e91fc23acd61431142f7d8
Sami Souayah <souays@bu.edu>
2024-12-07 23:52:01 -0500

    Assigns

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def get_all_favorites() -> Response:
         return make_response(jsonify({'error': str(e)}), 500)
 
 
-@app.route('/api/get-song-from-catalog-by-id/<int:song_id>', methods=['GET'])
+@app.route('/api/get_favorite_by_id', methods=['GET'])
 def get_favorite_by_ID(location_id: int) -> Response:
     """
     Route to retrieve a song by its ID.
