50a58d62bbdbd3b2785192a51686c72a198f75ff
Sami Souayah <souays@bu.edu>
2024-12-10 14:49:38 -0500

    Assigns

--- tests/test_favorite_locations_model.py
+++ tests/test_favorite_locations_model.py
@@ -14,14 +14,6 @@ def mock_update_play_count(mocker):
     """Mock the update_play_count function for testing purposes."""
     return mocker.patch("music_collection.models.playlist_model.update_play_count")
 
-"""Fixtures providing sample songs for the tests."""
-@pytest.fixture
-def sample_location():
-    return Song(1, 'Artist 1', 'Song 1', 2022, 'Pop', 180)
-
-@pytest.fixture
-def sample_song2():
-    return Song(2, 'Artist 2', 'Song 2', 2021, 'Rock', 155)
 
 @pytest.fixture
 def sample_playlist(sample_song1, sample_song2):
