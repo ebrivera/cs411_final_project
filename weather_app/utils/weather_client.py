f5f00f18411033310d431247730c6aaa99fb5d38
ebrivera <ebrivera@bu.edu>
2024-12-10 20:28:22 -0500

    updating weather_client

--- weather_app/utils/weather_client.py
+++ weather_app/utils/weather_client.py
@@ -3,10 +3,21 @@ import logging
 from typing import Any, Dict
 from dotenv import load_dotenv
 import os
-from weather_app.utils.logger import configure_logger
+from logger import configure_logger
+from geopy.geocoders import Nominatim
+
 
 
 load_dotenv()
+def get_lat_long(location_name):
+        geolocator = Nominatim(user_agent="my_geocoder")
+        location = geolocator.geocode(location_name)
+
+        if location:
+            return location.latitude, location.longitude
+        else:
+            return None
+
 
 
 class WeatherClient:
@@ -26,7 +37,8 @@ class WeatherClient:
         self.logger = logging.getLogger(__name__)
         configure_logger(self.logger)
 
-    def get_weather(self, location_name: str) -> Dict[str, Any]:
+
+    def get_weather(self, location_name: str):
         """
         Fetches weather data for a given location.
 
@@ -42,25 +54,23 @@ class WeatherClient:
         self.logger.info("Fetching weather data for location: %s", location_name)
 
         try:
+            url  = "https://api.openweathermap.org/data/3.0/onecall/overview?"
             # Construct the API request URL
+            latlong = get_lat_long(location_name)
             params = {
-                "q": location_name,
+                "lat": latlong[0],
+                "lon": latlong[1],
                 "appid": self.api_key,
-                "units": "metric",  # Use "imperial" for Fahrenheit
+                "units": "imperial",  # Use "imperial" for Fahrenheit
             }
-            response = requests.get(self.base_url, params=params)
+            response = requests.get(url, params=params)
             response.raise_for_status()  # Raise an HTTPError for bad responses
 
             # Parse and return the weather data
+            
             weather_data = response.json()
-            self.logger.info("Weather data for %s: %s", location_name, weather_data)
-            return {
-                "location": weather_data.get("name"),
-                "temperature": weather_data["main"]["temp"],
-                "description": weather_data["weather"][0]["description"],
-                "humidity": weather_data["main"]["humidity"],
-                "wind_speed": weather_data["wind"]["speed"],
-            }
+            self.logger.info("Weather data for %s fetched", location_name)
+            return f"Location: {location_name} \n Date: {weather_data['date']} \n Overview: {weather_data['weather_overview']}"
 
         except requests.exceptions.RequestException as e:
             self.logger.error("Failed to fetch weather data for %s: %s", location_name, str(e))
@@ -69,4 +79,138 @@ class WeatherClient:
         except KeyError as e:
             self.logger.error("Unexpected response structure: %s", str(e))
             raise ValueError("Unexpected response structure from weather API")
+        
+    def get_daily_forecast(self, location_name: str):
+        """
+        Fetches weather data for a given location.
+
+        Args:
+            location_name (str): The name of the location (e.g., city name).
+
+        Returns:
+            Dict[str, Any]: A dictionary containing weather data for the location.
+
+        Raises:
+            ValueError: If the API request fails or the location is not found.
+        """
+        self.logger.info("Fetching weather data for location: %s", location_name)
+
+        try:
+            url  = "https://api.openweathermap.org/data/3.0/onecall?"
+            # Construct the API request URL
+            latlong = get_lat_long(location_name)
+            params = {
+                "lat": latlong[0],
+                "lon": latlong[1],
+                "appid": self.api_key,
+                "exclude": "current,minutely,hourly",
+                "units": "imperial",  # Use "imperial" for Fahrenheit
+            }
+            response = requests.get(url, params=params)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+
+            # Parse and return the weather data
+            
+            weather_data = response.json()
+            self.logger.info("Weather data for %s fetched", location_name)
+            return f"Location: {location_name} \n High: {weather_data['daily'][0]['temp']['max']}F \n Low: {weather_data['daily'][0]['temp']['min']}F \n Humidity: {weather_data['daily'][0]['humidity']}% \n Weather: {weather_data['daily'][0]['weather'][0]['description']} \n Alerts: {weather_data['alerts'][0]['description']}"
+
+        except requests.exceptions.RequestException as e:
+            self.logger.error("Failed to fetch weather data for %s: %s", location_name, str(e))
+            raise ValueError(f"Error fetching weather data for location '{location_name}': {str(e)}")
+
+        except KeyError as e:
+            self.logger.error("Unexpected response structure: %s", str(e))
+            raise ValueError("Unexpected response structure from weather API")
+        
+    
+    def get_hourly_forecast(self, location_name: str):
+        """
+        Fetches weather data for a given location.
+
+        Args:
+            location_name (str): The name of the location (e.g., city name).
+
+        Returns:
+            Dict[str, Any]: A dictionary containing weather data for the location.
+
+        Raises:
+            ValueError: If the API request fails or the location is not found.
+        """
+        self.logger.info("Fetching weather data for location: %s", location_name)
+
+        try:
+            url  = "https://api.openweathermap.org/data/3.0/onecall?"
+            # Construct the API request URL
+            latlong = get_lat_long(location_name)
+            params = {
+                "lat": latlong[0],
+                "lon": latlong[1],
+                "appid": self.api_key,
+                "exclude": "current,minutely,daily",
+                "units": "imperial",  # Use "imperial" for Fahrenheit
+            }
+            response = requests.get(url, params=params)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+
+            # Parse and return the weather data
+            
+            weather_data = response.json()
+            self.logger.info("Weather data for %s fetched", location_name)
+            return f"Location: {location_name} \n High: {weather_data['hourly'][0]['temp']['max']}F \n Low: {weather_data['hourly'][0]['temp']['min']}F \n Humidity: {weather_data['hourly'][0]['humidity']}% \n Weather: {weather_data['hourly'][0]['weather'][0]['description']} \n Alerts: {weather_data['alerts'][0]['description']}"
+
+        except requests.exceptions.RequestException as e:
+            self.logger.error("Failed to fetch weather data for %s: %s", location_name, str(e))
+            raise ValueError(f"Error fetching weather data for location '{location_name}': {str(e)}")
+
+        except KeyError as e:
+            self.logger.error("Unexpected response structure: %s", str(e))
+            raise ValueError("Unexpected response structure from weather API")
+        
+    def get_date_forecast(self, location_name: str, date: str):
+        """
+        Fetches weather data for a given location.
+
+        Args:
+            location_name (str): The name of the location (e.g., city name).
+
+        Returns:
+            Dict[str, Any]: A dictionary containing weather data for the location.
+
+        Raises:
+            ValueError: If the API request fails or the location is not found.
+        """
+        self.logger.info("Fetching weather data for location: %s", location_name)
+
+        try:
+            url  = "https://api.openweathermap.org/data/3.0/onecall/day_summary?"
+            # Construct the API request URL
+            latlong = get_lat_long(location_name)
+            params = {
+                "lat": latlong[0],
+                "lon": latlong[1],
+                "appid": self.api_key,
+                "date": date,
+                "units": "imperial",  # Use "imperial" for Fahrenheit
+            }
+            response = requests.get(url, params=params)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+
+            # Parse and return the weather data
+            
+            weather_data = response.json()
+            self.logger.info("Weather data for %s fetched", location_name)
+            return f"Location: {location_name} \n Date: {weather_data['date']} (YYYY-MM-DD) \n High: {weather_data['temperature']['max']}F \n Low: {weather_data['temperature']['min']}F \n Precipitation: {weather_data['precipitation']['total']} inches \n Humidity: {weather_data['humidity']['afternoon']}%"
+
+        except requests.exceptions.RequestException as e:
+            self.logger.error("Failed to fetch weather data for %s: %s", location_name, str(e))
+            raise ValueError(f"Error fetching weather data for location '{location_name}': {str(e)}")
+
+        except KeyError as e:
+            self.logger.error("Unexpected response structure: %s", str(e))
+            raise ValueError("Unexpected response structure from weather API")
+
+#if __name__ == "__main__":
+    #client = WeatherClient()
+    #print(client.get_date_forecast("New York City","2024-9-10"))
 
